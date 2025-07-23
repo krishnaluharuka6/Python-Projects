20449689ab59f3202b3b34431426b9cbe389ec8e
krishnaluharuka6 <krishna.luharuka11@gmail.com>
2025-07-23 19:12:15 +0545

    Email validation using regex

--- email_validation_with_regex/script.py
+++ email_validation_with_regex/script.py
@@ -1,7 +1,22 @@
-var=[3,4]
+# a-z lower case
+# 0-9
+# . and _ only once before @
+# @ only one
+# . after @ index should be -3 or -4
 
-var2=var
 
-var2=[1,2]
 
-print(var,var2)
+# x =               r"^[a-z]+[\._]?[a-z0-9]+[@]\w+[\.]\w{2,3}$"
+
+
+
+import re
+
+email_condition = r"^[a-z]+[\._]?[a-z0-9]+[@]\w+[\.]\w{2,3}$"
+
+user_email = input("Enter your email")
+
+if re.search(email_condition,user_email):
+    print("Right email")
+else:
+    print("Wrong email")
